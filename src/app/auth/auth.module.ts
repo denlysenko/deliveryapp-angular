b4900728aca7cff488b782825c7558edc9806059
denlysenko <den.lysenko.mail@gmail.com>
2018-10-09 11:54:46 +0300

    auth state initialization

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { AuthService } from './services/auth.service';
+import { effects, reducers } from './store';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature('auth', reducers),
+    EffectsModule.forFeature(effects)
   ],
-  declarations: []
+  declarations: [],
+  providers: [AuthService]
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/store/effects/index.ts
+++ src/app/auth/store/effects/index.ts
@@ -1,3 +1,16 @@
+import { LoginEffects } from './login.effects';
+import { MessagesEffects } from './messages.effects';
+import { RegisterEffects } from './register.effects';
+import { SelfEffects } from './self.effects';
+
+export const effects: any[] = [
+  LoginEffects,
+  RegisterEffects,
+  SelfEffects,
+  MessagesEffects
+];
+
 export * from './login.effects';
 export * from './messages.effects';
 export * from './register.effects';
+export * from './self.effects';
