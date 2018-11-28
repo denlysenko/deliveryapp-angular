e03fbf46f5b5765cb35052f804f50c3078e05d44
denlysenko <den.lysenko.mail@gmail.com>
2018-11-28 18:12:04 +0200

    self actions types as enum

--- src/app/modules/auth/store/actions/self.actions.spec.ts
+++ src/app/modules/auth/store/actions/self.actions.spec.ts
@@ -1,12 +1,12 @@
 import { User } from '../../models';
-import * as fromSelf from './self.actions';
+import { LoadSelf, LoadSelfFail, LoadSelfSuccess, Logout, SelfActionTypes } from './self.actions';
 
 describe('Self Actions', () => {
   describe('LoadSelf', () => {
     it('should create an action', () => {
-      const action = new fromSelf.LoadSelf();
+      const action = new LoadSelf();
       expect({ ...action }).toEqual({
-        type: fromSelf.LOAD_SELF
+        type: SelfActionTypes.LOAD_SELF
       });
     });
   });
@@ -22,9 +22,9 @@ describe('Self Actions', () => {
         phone: '1(111) 111-11-11',
         role: 1
       };
-      const action = new fromSelf.LoadSelfSuccess(payload);
+      const action = new LoadSelfSuccess(payload);
       expect({ ...action }).toEqual({
-        type: fromSelf.LOAD_SELF_SUCCESS,
+        type: SelfActionTypes.LOAD_SELF_SUCCESS,
         payload: payload
       });
     });
@@ -33,9 +33,9 @@ describe('Self Actions', () => {
   describe('LoadSelfFail', () => {
     it('should create an action', () => {
       const payload = { message: 'Error message' };
-      const action = new fromSelf.LoadSelfFail(payload);
+      const action = new LoadSelfFail(payload);
       expect({ ...action }).toEqual({
-        type: fromSelf.LOAD_SELF_FAIL,
+        type: SelfActionTypes.LOAD_SELF_FAIL,
         payload: payload
       });
     });
@@ -43,9 +43,9 @@ describe('Self Actions', () => {
 
   describe('Logout', () => {
     it('should create an action', () => {
-      const action = new fromSelf.Logout();
+      const action = new Logout();
       expect({ ...action }).toEqual({
-        type: fromSelf.LOGOUT
+        type: SelfActionTypes.LOGOUT
       });
     });
   });
--- src/app/modules/auth/store/actions/self.actions.ts
+++ src/app/modules/auth/store/actions/self.actions.ts
@@ -2,29 +2,31 @@ import { Action } from '@ngrx/store';
 
 import { User } from '../../models';
 
-export const LOAD_SELF = '[Auth] Load Self';
-export const LOAD_SELF_SUCCESS = '[Auth] Load Self Success';
-export const LOAD_SELF_FAIL = '[Auth] Load Self Fail';
-export const LOGOUT = '[Auth] Logout';
+export enum SelfActionTypes {
+  LOAD_SELF = '[Auth] Load Self',
+  LOAD_SELF_SUCCESS = '[Auth] Load Self Success',
+  LOAD_SELF_FAIL = '[Auth] Load Self Fail',
+  LOGOUT = '[Auth] Logout'
+}
 
 export class LoadSelf implements Action {
-  readonly type = LOAD_SELF;
+  readonly type = SelfActionTypes.LOAD_SELF;
 }
 
 export class LoadSelfSuccess implements Action {
-  readonly type = LOAD_SELF_SUCCESS;
+  readonly type = SelfActionTypes.LOAD_SELF_SUCCESS;
 
   constructor(public payload: User) {}
 }
 
 export class LoadSelfFail implements Action {
-  readonly type = LOAD_SELF_FAIL;
+  readonly type = SelfActionTypes.LOAD_SELF_FAIL;
 
   constructor(public payload: any) {}
 }
 
 export class Logout implements Action {
-  readonly type = LOGOUT;
+  readonly type = SelfActionTypes.LOGOUT;
 }
 
 export type UserAction = LoadSelf | LoadSelfSuccess | LoadSelfFail | Logout;
