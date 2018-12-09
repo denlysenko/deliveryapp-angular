6df2fba6373372851ab332c74095c04d2cc0015b
denlysenko <den.lysenko.mail@gmail.com>
2018-12-09 23:01:42 +0200

    display topbar in app-shell

--- src/app/modules/app-shell/app-shell.module.ts
+++ src/app/modules/app-shell/app-shell.module.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { SidebarModule } from 'primeng/primeng';
+import { ButtonModule, InputTextModule, MenuModule, SidebarModule } from 'primeng/primeng';
 
 import { componentDeclarations, importDeclarations } from './app-shell.common';
+import { TopbarComponent } from './components';
 
 @NgModule({
-  imports: [CommonModule, ...importDeclarations, SidebarModule],
-  declarations: [...componentDeclarations]
+  imports: [
+    CommonModule,
+    ...importDeclarations,
+    SidebarModule,
+    InputTextModule,
+    MenuModule,
+    ButtonModule
+  ],
+  declarations: [...componentDeclarations, TopbarComponent]
 })
 export class AppShellModule {}
--- src/app/modules/app-shell/containers/app-shell/app-shell.component.ts
+++ src/app/modules/app-shell/containers/app-shell/app-shell.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+import { CoreFacade } from '@core/store';
+
 @Component({
   templateUrl: './app-shell.component.html',
   styleUrls: ['./app-shell.component.scss']
 })
 export class AppShellComponent {
   showMessages = false;
+  user$ = this.coreFacade.self$;
+  unreadMessages$ = this.coreFacade.unreadMessages$;
 
-  constructor() {}
+  constructor(private coreFacade: CoreFacade) {}
+
+  logout() {
+    this.coreFacade.logout();
+  }
 }
