4f87b0e2c5b0a732b1a2d06329b690d3b944607d
denlysenko <den.lysenko.mail@gmail.com>
2018-12-22 20:18:37 +0200

    [Mobile] hide action bar on auth page

--- src/app/modules/auth/containers/auth-page/auth-page.component.tns.ts
+++ src/app/modules/auth/containers/auth-page/auth-page.component.tns.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Page } from 'ui/page';
+
 import { AuthPageBase } from '../../base/AuthPageBase';
 import { AuthFacade } from '../../store/auth.facade';
 
 @Component({
+  moduleId: module.id,
   templateUrl: './auth-page.component.html',
   styleUrls: ['./auth-page.component.scss']
 })
 export class AuthPageComponent extends AuthPageBase {
-  constructor(authFacade: AuthFacade) {
+  constructor(authFacade: AuthFacade, private page: Page) {
     super(authFacade);
+    this.page.actionBarHidden = true;
   }
 }
