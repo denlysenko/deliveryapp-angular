1b801d9cc12bbeb90fb47ed5de5e91b07f0b023b
denlysenko <den.lysenko.mail@gmail.com>
2019-02-12 19:06:47 +0200

    add navigation to create order page

--- src/app/modules/orders/orders.routes.ts
+++ src/app/modules/orders/orders.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
+import { CreateOrderPageComponent } from './containers/create-order-page/create-order-page.component';
 import { OrdersPageComponent } from './containers/orders-page/orders-page.component';
 import { OrdersResolver } from './resolvers/orders.resolver';
+import { AuthGuard } from '@core/guards';
 
 export const routes: Routes = [
   {
@@ -9,6 +11,12 @@ export const routes: Routes = [
     component: OrdersPageComponent,
     resolve: {
       orders: OrdersResolver
-    }
+    },
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'create',
+    component: CreateOrderPageComponent,
+    canActivate: [AuthGuard]
   }
 ];
