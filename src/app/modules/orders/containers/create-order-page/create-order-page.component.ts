03b6cd00bad9a96d0ca2e729029091965d5782da
denlysenko <den.lysenko.mail@gmail.com>
2019-02-12 17:43:52 +0200

    generate create order page component

---
+++ src/app/modules/orders/containers/create-order-page/create-order-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'da-create-order-page',
+  templateUrl: './create-order-page.component.html',
+  styleUrls: ['./create-order-page.component.scss']
+})
+export class CreateOrderPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
