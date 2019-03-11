4971cbe1c3a9096bbca34a25bdff09fe194fd61c
denlysenko <den.lysenko.mail@gmail.com>
2019-03-11 22:21:32 +0200

    generate update order form component

---
+++ src/app/modules/orders/components/order-forms/update-order-form/update-order-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'da-update-order-form',
+  templateUrl: './update-order-form.component.html',
+  styleUrls: ['./update-order-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UpdateOrderFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/modules/orders/containers/index.ts
+++ src/app/modules/orders/containers/index.ts
@@ -1,5 +1,5 @@
-import { OrdersPageComponent } from './orders-page/orders-page.component';
 import { CreateOrderPageComponent } from './create-order-page/create-order-page.component';
+import { OrdersPageComponent } from './orders-page/orders-page.component';
 import { UpdateOrderPageComponent } from './update-order-page/update-order-page.component';
 
 export const containers: any[] = [
