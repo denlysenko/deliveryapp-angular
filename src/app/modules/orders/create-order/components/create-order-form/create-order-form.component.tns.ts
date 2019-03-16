be6db1a828ebc0310cd4cd0fb788dbfc38b38a53
denlysenko <den.lysenko.mail@gmail.com>
2019-03-16 22:32:22 +0200

    create order form layout

--- src/app/modules/orders/create-order/components/create-order-form/create-order-form.component.tns.ts
+++ src/app/modules/orders/create-order/components/create-order-form/create-order-form.component.tns.ts
@@ -1,4 +1,6 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { User } from '@auth/models';
+import { Roles } from '@common/enums';
 
 @Component({
   selector: 'da-create-order-form',
@@ -7,9 +9,35 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateOrderFormComponent {
+  readonly roles = Roles;
+
   order = {
-    name: 'Order test',
-    weight: 2,
-    from: 'Test'
+    cityFrom: null,
+    cityTo: null,
+    addressFrom: null,
+    addressTo: null,
+    additionalData: null,
+    cargoName: null,
+    cargoWeight: null,
+    cargoVolume: null,
+    comment: null,
+    senderCompany: null,
+    senderName: null,
+    senderEmail: null,
+    senderPhone: null
   };
+
+  @Input() role: number;
+
+  @Input()
+  set clients(clients: User[]) {
+    if (clients) {
+      this.clientsProvider = clients.map(client => ({
+        key: client.id,
+        label: client.email
+      }));
+    }
+  }
+
+  clientsProvider: { key: number; label: string }[];
 }
