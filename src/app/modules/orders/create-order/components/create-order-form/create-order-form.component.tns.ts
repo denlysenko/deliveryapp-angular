a16a20e8b904d0dd19c10fdfa558e9f2504365e3
denlysenko <den.lysenko.mail@gmail.com>
2019-03-19 16:24:07 +0200

    submit create order form

--- src/app/modules/orders/create-order/components/create-order-form/create-order-form.component.tns.ts
+++ src/app/modules/orders/create-order/components/create-order-form/create-order-form.component.tns.ts
@@ -1,6 +1,24 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild
+} from '@angular/core';
+
 import { User } from '@auth/models';
+
 import { Roles } from '@common/enums';
+import { ValidationError } from '@common/models';
+
+import { FeedbackService, LoaderService } from '@core/services';
+
+import { RadDataForm } from 'nativescript-ui-dataform';
+import { RadDataFormComponent } from 'nativescript-ui-dataform/angular';
+
+import { Order } from '../../../models';
+import { ERROR_MESSAGE } from '../../constants';
 
 @Component({
   selector: 'da-create-order-form',
@@ -11,23 +29,36 @@ import { Roles } from '@common/enums';
 export class CreateOrderFormComponent {
   readonly roles = Roles;
 
-  order = {
-    cityFrom: null,
-    cityTo: null,
-    addressFrom: null,
-    addressTo: null,
-    additionalData: null,
-    cargoName: null,
-    cargoWeight: null,
+  order: Order = {
+    cityFrom: '',
+    cityTo: '',
+    addressFrom: '',
+    addressTo: '',
+    additionalData: '',
+    cargoName: '',
+    cargoWeight: 0,
     cargoVolume: null,
-    comment: null,
-    senderCompany: null,
-    senderName: null,
-    senderEmail: null,
-    senderPhone: null
+    comment: '',
+    senderCompany: '',
+    senderName: '',
+    senderEmail: '',
+    senderPhone: ''
   };
 
+  clientsProvider: { key: number; label: string }[];
+
+  @ViewChild('dataForm') dataForm: RadDataFormComponent;
+
   @Input() role: number;
+
+  @Input()
+  set loading(isLoading: boolean) {
+    this._loading = isLoading;
+    this.loaderService[isLoading ? 'start' : 'stop']();
+  }
+  get loading(): boolean {
+    return this._loading;
+  }
 
   @Input()
   set clients(clients: User[]) {
@@ -39,5 +70,49 @@ export class CreateOrderFormComponent {
     }
   }
 
-  clientsProvider: { key: number; label: string }[];
+  @Input()
+  set error(error: ValidationError) {
+    if (error) {
+      this.handleError(error);
+    }
+  }
+
+  @Output() submitted = new EventEmitter<Order>();
+
+  private _loading = false;
+
+  constructor(
+    private feedbackService: FeedbackService,
+    private loaderService: LoaderService
+  ) {}
+
+  get dataform(): RadDataForm {
+    return this.dataForm.dataForm;
+  }
+
+  async submit() {
+    if (this.loading) {
+      return;
+    }
+
+    const isValid = await this.dataform.validateAll();
+
+    if (isValid) {
+      this.submitted.emit(this.order);
+    }
+  }
+
+  private handleError(err: ValidationError) {
+    if (err.errors) {
+      err.errors.forEach(({ path, message }) => {
+        const formControl = this.dataform.getPropertyByName(path);
+        if (formControl) {
+          formControl.errorMessage = message;
+          this.dataform.notifyValidated(path, false);
+        }
+      });
+    }
+
+    this.feedbackService.error(ERROR_MESSAGE);
+  }
 }
