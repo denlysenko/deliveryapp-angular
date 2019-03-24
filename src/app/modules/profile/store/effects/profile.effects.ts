2bc2ce1d073fc646d183c51a8d062561a2f4c821
denlysenko <den.lysenko.mail@gmail.com>
2019-03-24 22:37:14 +0200

    add profile effects

---
+++ src/app/modules/profile/store/effects/profile.effects.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+
+import { FeedbackService } from '@core/services';
+
+import { Actions, Effect, ofType } from '@ngrx/effects';
+
+import { of } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
+
+import { ProfileService } from '../../services/profile.service';
+import {
+  ProfileActionTypes,
+  UpdatePassword,
+  UpdatePasswordFail,
+  UpdatePasswordSuccess,
+  UpdateProfile,
+  UpdateProfileFail,
+  UpdateProfileSuccess
+} from '../actions';
+
+const PROFILE_UPDATED_MESSAGE = 'Profile updated';
+const PASSWORD_UPDATED_MESSAGE = 'Password updated';
+
+@Injectable()
+export class ProfileEffects {
+  constructor(
+    private profileService: ProfileService,
+    private actions$: Actions,
+    private feedbackService: FeedbackService
+  ) {}
+
+  @Effect()
+  updateProfile$ = this.actions$.pipe(
+    ofType(ProfileActionTypes.UPDATE_PROFILE),
+    map((action: UpdateProfile) => action.payload),
+    switchMap(profile =>
+      this.profileService.updateProfile(profile).pipe(
+        map(user => new UpdateProfileSuccess(user)),
+        catchError(err => of(new UpdateProfileFail(err)))
+      )
+    )
+  );
+
+  @Effect()
+  updatePassword$ = this.actions$.pipe(
+    ofType(ProfileActionTypes.UPDATE_PASSWORD),
+    map((action: UpdatePassword) => action.payload),
+    switchMap(passwordPayload =>
+      this.profileService.updatePassword(passwordPayload).pipe(
+        map(() => new UpdatePasswordSuccess()),
+        catchError(err => of(new UpdatePasswordFail(err)))
+      )
+    )
+  );
+
+  @Effect({ dispatch: false })
+  updateProfileSuccess$ = this.actions$.pipe(
+    ofType(ProfileActionTypes.UPDATE_PROFILE_SUCCESS),
+    tap(() => this.feedbackService.success(PROFILE_UPDATED_MESSAGE))
+  );
+
+  @Effect({ dispatch: false })
+  updatePasswordSuccess$ = this.actions$.pipe(
+    ofType(ProfileActionTypes.UPDATE_PASSWORD_SUCCESS),
+    tap(() => this.feedbackService.success(PASSWORD_UPDATED_MESSAGE))
+  );
+}
