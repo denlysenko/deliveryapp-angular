2023c4b00daf93f52c6aff523b5d2ea6f0a0917a
denlysenko <den.lysenko.mail@gmail.com>
2019-03-25 15:42:17 +0200

    remove profile store

--- src/app/modules/profile/store/reducers/index.ts
+++
@@ -1,5 +0,0 @@
-import { createFeatureSelector } from '@ngrx/store';
-import { ProfileState } from './profile.reducer';
-
-export { profileReducer, ProfileState } from './profile.reducer';
-export const getProfileState = createFeatureSelector<ProfileState>('profile');
