e5a4b86307038d1d4776fbd771a7693242bc3b35
denlysenko <den.lysenko.mail@gmail.com>
2019-04-13 16:49:48 +0300

    add payments page skeleton

---
+++ src/app/modules/payments/containers/payments-page/payments-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './payments-page.component.html',
+  styleUrls: ['./payments-page.component.scss']
+})
+export class PaymentsPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/modules/payments/payments.module.ts
+++ src/app/modules/payments/payments.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { PaymentsPageComponent } from './containers/payments-page/payments-page.component';
+import { PaymentsRoutingModule } from './payments-routing.module';
 import { importDeclarations, providerDeclarations } from './payments.common';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, ...importDeclarations],
+  declarations: [PaymentsPageComponent],
+  imports: [CommonModule, PaymentsRoutingModule, ...importDeclarations],
   providers: [...providerDeclarations]
 })
 export class PaymentsModule {}
