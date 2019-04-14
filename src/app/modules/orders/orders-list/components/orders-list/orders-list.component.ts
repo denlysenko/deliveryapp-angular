3eebbc87d0268db7b7e9915ab68130b9213a90d9
denlysenko <den.lysenko.mail@gmail.com>
2019-04-14 14:18:42 +0300

    extend orders list from base list

--- src/app/modules/orders/orders-list/components/orders-list/orders-list.component.ts
+++ src/app/modules/orders/orders-list/components/orders-list/orders-list.component.ts
@@ -1,17 +1,9 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { BaseListComponent } from '@base/BaseListComponent';
 
 import { ORDER_STATUSES } from '@common/constants';
 import { Roles } from '@common/enums';
-import { PageChangeEvent, SortingChangeEvent } from '@common/models';
-import { extractSortFieldAndOrder } from '@common/utils';
-import { SortEvent } from 'primeng/primeng';
 
 import { Order } from '../../../models';
 
@@ -21,38 +13,10 @@ import { Order } from '../../../models';
   styleUrls: ['./orders-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OrdersListComponent implements OnInit {
+export class OrdersListComponent extends BaseListComponent {
   readonly statuses = ORDER_STATUSES;
   readonly roles = Roles;
 
-  sortField: string;
-  sortOrder: number;
-
   @Input() role: number;
   @Input() orders: Order[];
-  @Input() count: number;
-  @Input() sorting: SortingChangeEvent;
-  @Input() pagination: PageChangeEvent;
-
-  @Output() sortingChanged = new EventEmitter<SortingChangeEvent>();
-  @Output() paginationChanged = new EventEmitter<PageChangeEvent>();
-
-  ngOnInit() {
-    const { sortField, sortOrder } = extractSortFieldAndOrder(this.sorting);
-    this.sortField = sortField;
-    this.sortOrder = sortOrder;
-  }
-
-  sort(event: SortEvent) {
-    this.sortingChanged.emit({
-      [`order[${event.field}]`]: event.order === 1 ? 'asc' : 'desc'
-    });
-  }
-
-  paginate(event: any) {
-    this.paginationChanged.emit({
-      limit: event.rows,
-      offset: event.rows * event.page
-    });
-  }
 }
