48421c3e6a7863648833f126cf5fab13656355eb
denlysenko <den.lysenko.mail@gmail.com>
2019-04-20 20:05:05 +0300

    generate payment page

---
+++ src/app/modules/payments/containers/payment-page/payment-page.component.tns.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './payment-page.component.tns.html',
+  styleUrls: ['./payment-page.component.scss']
+})
+export class PaymentPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
