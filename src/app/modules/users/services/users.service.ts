8f34265d3b835f50a86b90a4fe1826d268b90e56
denlysenko <den.lysenko.mail@gmail.com>
2019-04-30 17:21:51 +0300

    add users service

--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -4,7 +4,7 @@ import { ListResponse } from '@common/models';
 
 import { ApiService } from '@core/services/api.service';
 
-import { User } from '@users/models';
+import { User, UsersFilter } from '../models';
 
 import { Observable } from 'rxjs';
 
@@ -12,7 +12,19 @@ import { Observable } from 'rxjs';
 export class UsersService {
   constructor(private apiService: ApiService) {}
 
-  getUsers(query: any): Observable<ListResponse<User>> {
+  getUsers(query?: UsersFilter): Observable<ListResponse<User>> {
     return this.apiService.get('/users', query);
+  }
+
+  getById(id: number): Observable<User> {
+    return this.apiService.get(`/users/${id}`);
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.apiService.post('/users', user);
+  }
+
+  updateUser(userId: number, user: User): Observable<User> {
+    return this.apiService.patch(`/users/${userId}`, user);
   }
 }
