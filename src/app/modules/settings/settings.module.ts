0710a700513bd65cedc0e1f14291469ba7ed8a73
denlysenko <den.lysenko.mail@gmail.com>
2019-05-01 16:14:41 +0300

    add settings resolver

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SettingsResolver } from './resolvers/settings.resolver';
 import { SettingsService } from './services/settings.service';
 import { SettingsRoutingModule } from './settings-routing.module';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, SettingsRoutingModule],
-  providers: [SettingsService]
+  providers: [SettingsService, SettingsResolver]
 })
 export class SettingsModule {}
