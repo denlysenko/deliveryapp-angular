f331bfa2db650c97695d51b36880f23d6799bab8
denlysenko <den.lysenko.mail@gmail.com>
2019-05-17 21:45:38 +0300

    add logs list component

--- src/app/modules/logs/components/index.ts
+++ src/app/modules/logs/components/index.ts
@@ -1,3 +1,4 @@
 import { LogsFilterComponent } from './logs-filter/logs-filter.component';
+import { LogsListComponent } from './logs-list/logs-list.component';
 
-export const components: any[] = [LogsFilterComponent];
+export const components: any[] = [LogsFilterComponent, LogsListComponent];
---
+++ src/app/modules/logs/components/logs-list/logs-list.component.spec.ts
@@ -0,0 +1,69 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogsListComponent } from './logs-list.component';
+
+describe('LogsListComponent', () => {
+  let component: LogsListComponent;
+  let fixture: ComponentFixture<LogsListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [LogsListComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogsListComponent);
+    component = fixture.componentInstance;
+    component.sorting = {
+      'order[createdAt]': 'asc'
+    };
+    component.pagination = {
+      limit: 10,
+      offset: 0
+    };
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set correct sortField and sortOrder properties', () => {
+    expect(component.sortField).toBe('createdAt');
+    expect(component.sortOrder).toBe(1);
+  });
+
+  describe('sort()', () => {
+    it('should emit sortingChanged event', () => {
+      spyOn(component.sortingChanged, 'emit');
+
+      component.sort({
+        field: 'createdAt',
+        order: -1
+      });
+
+      expect(component.sortingChanged.emit).toHaveBeenCalledWith({
+        'order[createdAt]': 'desc'
+      });
+    });
+  });
+
+  describe('paginate()', () => {
+    it('should emit paginationChanged event', () => {
+      spyOn(component.paginationChanged, 'emit');
+
+      component.paginate({
+        rows: 10,
+        page: 2
+      });
+
+      expect(component.paginationChanged.emit).toHaveBeenCalledWith({
+        limit: 10,
+        offset: 20
+      });
+    });
+  });
+});
