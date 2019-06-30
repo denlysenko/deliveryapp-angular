cddf4aa3e74fcfc368d01719c1371bba56622e83
denlysenko <den.lysenko.mail@gmail.com>
2019-06-30 16:13:35 +0300

    move common component logic into base class

--- src/app/modules/messages/components/messages/messages.component.tns.ts
+++ src/app/modules/messages/components/messages/messages.component.tns.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 import { ModalDialogParams } from 'nativescript-angular/modal-dialog';
+
+import { takeUntil } from 'rxjs/operators';
+
+import { BaseMessagesComponent } from '../../base/BaseMessagesComponent';
+import { MessagesFacade } from '../../store';
 
 @Component({
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss']
 })
-export class MessagesComponent {
-  messages$ = this.params.context.messages$;
+export class MessagesComponent extends BaseMessagesComponent implements OnInit {
+  constructor(
+    messagesFacade: MessagesFacade,
+    private params: ModalDialogParams,
+    private cdr: ChangeDetectorRef
+  ) {
+    super(messagesFacade);
+  }
 
-  constructor(private params: ModalDialogParams) {}
+  ngOnInit() {
+    this.messagesFacade.messages$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(messages => {
+        this.messages = messages;
+        // in some cases, when message is received via push,
+        // list view is not updating. forcing this update by this call
+        this.cdr.detectChanges();
+      });
+
+    this.messagesFacade.totalCount$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(count => (this.count = count));
+  }
 
   close() {
     this.params.closeCallback(null);
