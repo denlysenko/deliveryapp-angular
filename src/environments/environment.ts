43be0c43b4af0ac82445844f964f02284e78071e
denlysenko <den.lysenko.mail@gmail.com>
2019-06-30 16:15:11 +0300

    change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://sheltered-escarpment-55326.herokuapp.com'
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://sheltered-escarpment-55326.herokuapp.com',
   firebaseAppId: '1:910231972624:web:a6d5902a751779b2',
   firebaseSenderId: '910231972624',
   firebasePublicKey:
